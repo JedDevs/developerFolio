75e6ae6795728b89750d57a074274e48e709d52e
Teotcd <theo.dockingtcd@gmail.com>
2020-12-20 17:57:05 +0000

    Merge branch 'master' of https://github.com/Teotcd/developerFolio into master

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,9 +7,9 @@ import emoji from "react-easy-emoji";
 
 const greeting = {
   /* Your Summary And Greeting Section */
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
-  subTitle: emoji("A passionate Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."),
+  username: "Theo Docking",
+  title: "Hi all, I'm Theo",
+  subTitle: emoji("A passionate Game Developer 🚀 with experience building games on the roblox and unreal platform with Lua / Knit and some other cool libraries and frameworks."),
   resumeLink: "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing"
 };
 
