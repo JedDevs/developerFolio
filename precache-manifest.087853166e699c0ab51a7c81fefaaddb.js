102094143a0884ebfea77aec127133bd36269b14
Teotcd <theo.dockingtcd@gmail.com>
2020-12-21 16:13:59 +0000

    addNew

---
+++ precache-manifest.087853166e699c0ab51a7c81fefaaddb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34a6061c57212304c045a1e32f8af6c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0492f95628965441a1f",
+    "url": "/static/css/main.82d2ff1b.chunk.css"
+  },
+  {
+    "revision": "9b7abf66277531c9051e",
+    "url": "/static/js/2.c3cc4726.chunk.js"
+  },
+  {
+    "revision": "b0492f95628965441a1f",
+    "url": "/static/js/main.51ee8ad3.chunk.js"
+  },
+  {
+    "revision": "ab12d3b252ae8aff746f",
+    "url": "/static/js/runtime-main.e484cc80.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "c442c93208c931e813f7343e0bdb06b4",
+    "url": "/static/media/GkfuWn0d_400x400.c442c932.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "654f048d9f3e6b65559bd1a065168d40",
+    "url": "/static/media/ae8b7b0e-c88b-45d9-bf26-aa5fb7c1f578-Org-OCR-Logo.654f048d.jpg"
+  },
+  {
+    "revision": "313906899525402e398061f0f3398e7c",
+    "url": "/static/media/black_tilt_1200x1200.31390689.png"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "596cf8cf23421133c83d273d03c306ce",
+    "url": "/static/media/pearson.596cf8cf.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/static/media/talksCardBack.de3da806.svg"
+  }
+]);
